0a6d3b638a835919b43e9e65e074fb0accfed0a4
pragatimindoutin <harshal.ladhe.1@gmail.com>
2022-09-02 15:44:58 +0530

    Init commit

---
+++ lib/demo_calcy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "demo_calcy/version"
+
+module DemoCalcy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/demo_calcy/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DemoCalcy
+  VERSION = "0.1.0"
+end
